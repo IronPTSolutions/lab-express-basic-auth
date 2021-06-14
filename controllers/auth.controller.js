3643463f957c1692c5a0668bc6a27cced358d8db
Your Name <sofia.gobar@gmail.com>
2021-06-14 17:58:42 +0200

    Iteration 1

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,32 +1,40 @@
 const mongoose = require("mongoose");
-const User = require("../models/User.model");
+const User = require("../models/user.model");
 
 module.exports.register = (req, res, next) => {
-  res.render("auth/register");
+  res.render('auth/register');
 };
 
 module.exports.doRegister = (req, res, next) => {
-  const user = ({ username, password } = req.body); ///viene del form, de la petición post
-  User.create(user)
-    .then((user) => res.redirect("/")) //tendrá que ir a login
-    .catch((error) => {
-      if (error instanceof mongoose.Error.ValidationError) {
-        res.render("auth/register", {
-          user: user, //para autocompletar la info que ya había escrito antes
-          errors: error.errors,
-        });
+
+  function renderWithErrors(errors) {
+    res.render('auth/register', {
+      user: req.body,
+      errors: errors
+    });
+  }
+
+
+  User.findOne({ username: req.body.username })
+    .then(user => {
+      if (user) {
+        renderWithErrors({username: 'Username already registered'})
+      } else {                                          //si no existe el usuario, tengo que crearlo
+        user = {username, password} = req.body;         //va sin const xk ya existe esa constante, entonces se pone así para 'destruirla' como si fuese un let
+        return User.create(user)                        //esto devuelve promesa, para que compartan mismo catch, poner el return, mismo camino al error
+        .then(user => res.redirect('/'))
+      }
+    })
+    .catch(error => {
+      if (errr instanceof mongoose.Error.ValidationError) {
+        renderWithErrors(error.errors);
       } else {
         next(error);
       }
-    });
+    })
 };
 
 /*
-if (error.name === 'MongoError' && error.code === 11000){
-        res.render("auth/register", {
-            user: user, //para autocompletar la info que ya había escrito antes
-            errors: MongoError,
-          });
-      }
-      
-      */
+//le paso los errores al usuario, pinto la vista primera y luego le paso lo que quiero que aparezca
+//errors es un objeto, asi que le paso una clave-valor
+*/
--- models/User.model.js
+++ models/User.model.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const PASSWORD_PATTERN = /^.{8,}$/i;
+const SALT_ROUNDS = 10;
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
   username: {
     type: String,
-    unique: [true, 'Username already registered' ],
+    unique: true,
     required: 'Username is required', 
     minLength: [3, 'Username needs at least 3 chars']
   },
@@ -18,5 +20,24 @@ const userSchema = new Schema({
   }
 });
 
+userSchema.pre('save', function (next) {
+  const user = this;                //como recordatorio de que se puede usar el this
+
+  if (user.isModified('password')) {              ///campo password
+    bcrypt.hash(user.password, SALT_ROUNDS)       //devuelve promesa
+      .then(hash => {
+        user.password = hash;   //listo para guardar en la db la password ya hasheada
+        next();                 //pasa a la base de datos 
+      })
+      .catch(error => next(error))
+  } else {
+    next();
+  }
+});
+
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
+
+
+//validaciones de mongoose, no va al mongo hasta que no compuebe todo eso
